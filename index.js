94e7502f4608de2018ab219bf5686ffae329d5d0
Nikhil Gangrade <nikhil12gangrade93@yahoo.com>
2018-09-18 18:51:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ function moveDodgerLeft() {
    */
   window.requestAnimationFrame(function(){
     const left = positionToInteger(DODGER.style.left)
-    if (left>0){
+    if (left > 0){
       DODGER.style.left = `{left - 4}px`
     }
   });
